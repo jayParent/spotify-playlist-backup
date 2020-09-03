c157f3f128d35e0dcf931fe09be3ecd3715ac267
Jerome <jeromeparent9@gmail.com>
2020-09-03 16:20:52 -0400

    saves all your playlists, need to get around spotify limit of 100 tracks per playlist

--- request.py
+++ request.py
@@ -8,6 +8,19 @@ scope = 'playlist-read-private'
 redirect_uri = 'http://localhost:8888'
 
 
+class Playlist:
+    def __init__(self, name, id, trackList):
+        self.name = name
+        self.id = id
+        self.trackList = trackList
+
+
+def main():
+    playlistsNamesAndIds = getPlaylists()
+    playlists = getTracksFromPlaylists(playlistsNamesAndIds)
+    # writeToCsv(playlists)
+
+
 def getAuthorization():
     auth_url = 'https://accounts.spotify.com/authorize?client_id=' + client_id + \
         '&redirect_uri=' + redirect_uri + '&scope=' + \
@@ -19,7 +32,7 @@ def getAuthorization():
 
 def getAccessAndRefreshToken():
     grant_type = 'authorization_code'
-    code = ''
+    code = 'AQCLAnVXfOi--DMI_W48dCE9QplGDIJo1FqDFgx3TVoBlsRyWg0VkFZoZXTkpV-U2rbh-2pi-Gh7EpCpNqO1UxIhhXCqz0OVYGpwIRrlTI-XTX3k72GvRVkz92kh22C0akhfzumJ5571mkjUV3KU-Ur_-VJS8wc0uPIunSXcEToFVjGjZA78MALEV2_FrnWK'
     token_url = 'https://accounts.spotify.com/api/token'
     headers = {'client_id': client_id, 'client_secret': client_secret,
                'grant_type': grant_type, 'code': code, 'redirect_uri': redirect_uri}
@@ -43,23 +56,49 @@ def getPlaylists():
     url = 'https://api.spotify.com/v1/me/playlists'
     access_token = getNewAccessToken()
     authorization = {'Authorization': 'Bearer ' + access_token}
-    trance_PlaylistId = '4NGcaASBaU1elQauvK65Z1'
 
-    url = 'https://api.spotify.com/v1/playlists/' + \
-        trance_PlaylistId + '/tracks?fields=items(track(name))'
     r = requests.get(url, headers=authorization)
-    jsonResponse = r.json()
 
-    return jsonResponse
+    playlists = r.json()
+    playlistsNamesAndIds = []
 
-def writeToCsv(trackList):
-    with open('trance_playlist.csv', mode='w') as csv_file:
-        writer = csv.writer(csv_file)
+    for playlist in playlists['items']:
+        pl = Playlist(playlist['name'], playlist['id'], '')
+        playlistsNamesAndIds.append(pl)
 
-        for track in trackList['items']:
-            writer.writerow([track['track']['name']])
-            print(track['track']['name'])
+    return playlistsNamesAndIds
 
 
-trackList = getPlaylists()
-writeToCsv(trackList)
+def getTracksFromPlaylists(playlistsNamesAndIds):
+    playlists = []
+    access_token = getNewAccessToken()
+    authorization = {'Authorization': 'Bearer ' + access_token}
+
+    for playlist in playlistsNamesAndIds:
+        url = 'https://api.spotify.com/v1/playlists/' + \
+            playlist.id + '/tracks?fields=items(track(name))'
+        r = requests.get(url, headers=authorization)
+        trackList = r.json()
+        print(type(trackList))
+
+        pl = Playlist(playlist.name, playlist.id, trackList)
+        playlists.append(pl)
+
+    return playlists
+
+
+def writeToCsv(playlists):
+    for playlist in playlists:
+        fileName = playlist.name + '.csv'
+        trackList = playlist.trackList
+        print(playlist.name, playlist.id)
+
+        with open(fileName, mode='w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+
+            for track in trackList['items']:
+                writer.writerow([track['track']['name']])
+
+
+if __name__ == "__main__":
+    main()
